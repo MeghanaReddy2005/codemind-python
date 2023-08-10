98d6a82c47d9588ed8ce241488479fa129065e4f
Meghana <119720532+MeghanaReddy2005@users.noreply.github.com>
2023-08-10 14:07:00 +0530

    a=int(input()) b=int(input()) print(F"Sum:{a+b}",end=' ') print(F"Difference:{a-b}",end=' ') print(F"Product:{a*b}",end=' ') print(F"Quotient:{a//b}",end=' ') print(F"Remainder:{a%b}",end=' ')

---
+++ Arithmetic_operations.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=int(input())
+print(F"Sum:{a+b}",end='
+')
+print(F"Difference:{a-b}",end='
+')
+print(F"Product:{a*b}",end='
+')
+print(F"Quotient:{a//b}",end='
+')
+print(F"Remainder:{a%b}",end='
+')
