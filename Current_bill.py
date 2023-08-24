816d17adffe8af2a0f5144d6ce0405ab76978ea8
Meghana <119720532+MeghanaReddy2005@users.noreply.github.com>
2023-08-24 15:11:37 +0530

    u=int(input()) if (u<=199):     c=1.20 elif (u>=200 and u<400):     c=1.50 elif (u>=400 and u<600):     c=1.80 elif (u>600):     c=2.00 b=u*c if (b>400):     sr=0.15*b else:     sr=100 cb=b+sr print(F"{cb:.2f}")

---
+++ Current_bill.py
@@ -0,0 +1,17 @@
+u=int(input())
+if (u<=199):
+    c=1.20
+elif (u>=200 and u<400):
+    c=1.50
+elif (u>=400 and u<600):
+    c=1.80
+elif (u>600):
+    c=2.00
+b=u*c
+if (b>400):
+    sr=0.15*b
+else:
+    sr=100
+cb=b+sr
+print(F"{cb:.2f}")
+
